8c07d871d583da894d24673245c14b52cb2968dc
tawhidtasin77 <tawhidtasin1762@gmail.com>
2026-06-02 18:58:00 +0600

    edited some text's

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,9 @@ function App() {
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
-            {/* Todo form goes here */}
             <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
             {
               (todos.length === 0)?
                 (<p className='w-full text-center text-gray-400'>No To-Do's yet!</p>):
--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -39,7 +39,6 @@ function TodoItem({ todo }) {
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
             />
-            {/* Edit, Save Button */}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
                 onClick={() => {
@@ -53,7 +52,6 @@ function TodoItem({ todo }) {
             >
                 {isTodoEditable ? "📁" : "✏️"}
             </button>
-            {/* Delete Todo Button */}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
                 onClick={() => deleteTodo(todo.id)}
